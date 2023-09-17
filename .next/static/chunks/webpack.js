f8248059bcdf8ae961f2a6ffc932f61b3f8f7b6b
Marcos Panontin <amarcospanontin@gmail.com>
2023-09-17 11:42:09 -0300

    docs: updates Readme.md

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4cdfb70096f54416"; }
+/******/ 		__webpack_require__.h = function() { return "39310b0f30d0c94c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
